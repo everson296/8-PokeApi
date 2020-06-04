ba7316f3d4e78d5a621b223cc81c4513d600ada0
Everson <tutinha296@gmal.com>
2020-06-03 23:20:25 -0300

    adicionando mais informações para o README

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
+~~~javascript
 const search = document.getElementById("pesquisar");
 const card = document.getElementById("listaDePokemons");
 const body = document.getElementById("body");
 const url = "https://pokeapi.co/api/v2";
 const pokemon = "pokemon";
+~~~
 
-
+~~~javascript    
 async function getPoke(user){
     const cardResponse = await fetch(`${url}/${pokemon}/${user}`);
     const card = await cardResponse.json();
 
     return {card};
 };
+~~~
 
+~~~javascript
 const showcard = (user) =>{
 
     card.innerHTML+=`
@@ -23,6 +27,7 @@ const showcard = (user) =>{
         </ul>
     `;
 }
+~~~
 
 const loadPokemon = async() => {
     for(let i = 1; i <= 151; i++ ){
@@ -31,87 +36,6 @@ const loadPokemon = async() => {
 
         showcard(card);
     }
-}
-
-const statusPokemon = () => {
-    card=`
-    <div id="cardStatusPokemon">
-    <div id="containerPokePerfil">
-        <div id="imgPokemon">
-            <div id="pokeImg"></div>
-            <div id="estiloImg">
-                <div id="estiloEscolha"></div>
-                <div id="estiloEscolha"></div>
-                <div id="estiloEscolha"></div>
-                <div id="estiloEscolha"></div>
-            </div>
-        </div>
-        <div id="idPokemon">#001</div>
-        <div id="nomePokemon">bulbasaur</div>
-    </div>
-    
-    <div id="containerPokeStatus">
-        <div id="containerTipo">
-            
-            <div id="tipo2">poison</div>
-            <div id="tipo1">grass</div>
-            <div id="fechar">X</div>
-        </div>
-        
-        <div id="containerStatus">
-            <div id="containerStatusBar">
-                <div class="statusName">HP</div>
-                <div class="statusNumber"></div>
-                <div class="statusBar">
-                    <div></div>
-                </div>
-            </div>
-            
-            <div id="containerStatusBar">
-                <div class="statusName">Attack</div>
-                <div class="statusNumber"></div>
-                <div class="statusBar">
-                    <div></div>
-                </div>
-            </div>
-            
-            <div id="containerStatusBar">
-                <div class="statusName">Defense</div>
-                <div class="statusNumber">260</div>
-                <div class="statusBar">
-                    <div></div>
-                </div>
-            </div>
-            
-            <div id="containerStatusBar">
-                <div class="statusName">Sp. Atk</div>
-                <div class="statusNumber"></div>
-                <div class="statusBar">
-                    <div></div>
-                </div>
-            </div>
-            
-            <div id="containerStatusBar">
-                <div class="statusName">Sp. Def</div>
-                <div class="statusNumber">15</div>
-                <div class="statusBar">
-                    <div></div>
-                </div>
-            </div>
-            
-            <div id="containerStatusBar">
-                <div class="statusName">Speed</div>
-                <div class="statusNumber">60</div>
-                <div class="statusBar">
-                    <div></div>
-                </div>
-            </div>
-            
-        </div>
-        
-    </div>
-</div>
-    `;
 }
 
 search.addEventListener("keyup", (e) =>{
